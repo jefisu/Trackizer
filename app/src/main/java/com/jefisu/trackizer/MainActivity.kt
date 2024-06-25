91bf1e51991962edcfe9dd3cd74a47fd6e624f8c
Jeferson Coutinho <83792093+jefisu@users.noreply.github.com>
2024-06-25 20:30:38 -0300

    add splash screen (#1)

--- app/src/main/java/com/jefisu/trackizer/MainActivity.kt
+++ app/src/main/java/com/jefisu/trackizer/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jefisu.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AppTheme(
