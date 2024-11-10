7c6b925b88c1d3abb59769eb85b1fed13d9ad2b0
Jeferson Coutinho <jefissuu@gmail.com>
2024-11-10 15:13:50 -0300

    fix: reset option picker state when hidden

--- core/presentation/designsystem/src/main/java/com/jefisu/designsystem/components/TrackizerOptionPicker.kt
+++ core/presentation/designsystem/src/main/java/com/jefisu/designsystem/components/TrackizerOptionPicker.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,6 +36,12 @@ fun <T> TrackizerOptionPicker(
         startIndex = startIndex,
     )
     val sheetState = rememberModalBottomSheetState()
+
+    LaunchedEffect(visible) {
+        if (!visible && pickerState.selectedIndex != startIndex) {
+            pickerState.setIndex(startIndex)
+        }
+    }
 
     TrackizerBottomSheet(
         isVisible = visible,
