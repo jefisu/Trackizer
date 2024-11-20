efc8321d2d5e021a12e1b20ee741c57e52804498
Jeferson Coutinho <jefissuu@gmail.com>
2024-11-20 20:23:25 -0300

    fix(ui): disable light status and navigation bars

--- core/presentation/designsystem/src/main/java/com/jefisu/designsystem/Theme.kt
+++ core/presentation/designsystem/src/main/java/com/jefisu/designsystem/Theme.kt
@@ -1,15 +1,30 @@
 package com.jefisu.designsystem
 
+import android.app.Activity
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 object TrackizerTheme
 
 @Composable
 fun TrackizerTheme(content: @Composable () -> Unit) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).apply {
+                isAppearanceLightStatusBars = false
+                isAppearanceLightNavigationBars = false
+            }
+        }
+    }
+
     CompositionLocalProvider(
         LocalTextStyle provides TrackizerTheme.typography.bodyLarge,
     ) {
