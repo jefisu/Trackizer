7988dc08aa716ae151780a79cb04d41544fdf080
Jeferson Coutinho <jefissuu@gmail.com>
2024-12-15 19:06:00 -0300

    refactor: Update SettingOptions component

--- feature/settings/src/main/java/com/jefisu/settings/presentation/components/SettingOptions.kt
+++ feature/settings/src/main/java/com/jefisu/settings/presentation/components/SettingOptions.kt
@@ -3,15 +3,17 @@ package com.jefisu.settings.presentation.components
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
@@ -20,11 +22,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.jefisu.designsystem.BorderBrush
 import com.jefisu.designsystem.Gray30
 import com.jefisu.designsystem.Gray60
+import com.jefisu.designsystem.R as DesignSystemRes
 import com.jefisu.designsystem.TrackizerTheme
 import com.jefisu.designsystem.components.TrackizerSwitch
 import com.jefisu.designsystem.size
@@ -33,13 +38,20 @@ import com.jefisu.designsystem.typography
 import com.jefisu.designsystem.util.rippleClickable
 import com.jefisu.settings.R
 import com.jefisu.ui.screen.LocalScreenIsSmall
-import com.jefisu.designsystem.R as DesignSystemRes
+
+internal data class SettingOption(
+    @DrawableRes val icon: Int,
+    val title: String,
+    val settingSelected: String? = null,
+    val onClick: (() -> Unit)? = null,
+    val leadingContent: @Composable (() -> Unit)? = null,
+)
 
 @Composable
 internal fun SettingOptions(
     title: String,
+    options: List<SettingOption>,
     modifier: Modifier = Modifier,
-    options: @Composable ColumnScope.() -> Unit,
 ) {
     Column(
         modifier = modifier,
@@ -49,44 +61,60 @@ internal fun SettingOptions(
             style = TrackizerTheme.typography.headline2,
         )
         Spacer(Modifier.height(TrackizerTheme.spacing.small))
-        Column(
-            verticalArrangement = Arrangement.spacedBy(TrackizerTheme.spacing.medium),
-            modifier = Modifier
-                .background(Gray60.copy(0.2f), Shapes().large)
-                .border(
-                    width = 1.dp,
-                    brush = BorderBrush,
-                    shape = Shapes().large,
-                )
-                .padding(
-                    horizontal = TrackizerTheme.spacing.extraMedium,
-                    vertical = if (LocalScreenIsSmall.current) {
-                        TrackizerTheme.spacing.extraSmall
-                    } else TrackizerTheme.spacing.medium,
-                ),
-        ) {
-            options()
+        BoxWithConstraints {
+            LazyColumn(
+                userScrollEnabled = false,
+                modifier = Modifier
+                    .heightIn(max = maxHeight)
+                    .background(Gray60.copy(0.2f), Shapes().large)
+                    .border(
+                        width = 1.dp,
+                        brush = BorderBrush,
+                        shape = Shapes().large,
+                    ),
+            ) {
+                itemsIndexed(
+                    items = options,
+                    key = { index, _ -> index },
+                ) { index, settingOption ->
+                    val calculatePadding = @Composable { isDefaultPadding: Boolean ->
+                        val defaultPadding = if (LocalScreenIsSmall.current) {
+                            TrackizerTheme.spacing.medium
+                        } else 20.dp
+                        when {
+                            options.size == 1 -> defaultPadding
+                            isDefaultPadding -> defaultPadding
+                            else -> defaultPadding * 0.65f
+                        }
+                    }
+                    SettingOptionItem(
+                        option = settingOption,
+                        modifier = Modifier
+                            .rippleClickable(
+                                enabled = settingOption.onClick != null,
+                                onClick = { settingOption.onClick?.invoke() },
+                            )
+                            .padding(
+                                start = TrackizerTheme.spacing.extraMedium,
+                                end = TrackizerTheme.spacing.extraMedium,
+                                top = calculatePadding(index == 0),
+                                bottom = calculatePadding(index == options.lastIndex),
+                            ),
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
-internal fun SettingOptionItem(
-    @DrawableRes icon: Int,
-    title: String,
+private fun SettingOptionItem(
+    option: SettingOption,
     modifier: Modifier = Modifier,
-    onClick: (() -> Unit)? = null,
-    leadingContent: @Composable (() -> Unit)? = null,
-    settingSelected: String? = null,
-) {
+) = with(option) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .height(32.dp)
-            .rippleClickable(
-                enabled = onClick != null,
-                onClick = { onClick?.invoke() },
-            ),
+        modifier = modifier,
     ) {
         Icon(
             painter = painterResource(icon),
@@ -124,20 +152,20 @@ internal fun SettingOptionItem(
 private fun SettingOptionsPreview() {
     TrackizerTheme {
         SettingOptions(
-            title = "General",
-            modifier = Modifier
-                .padding(TrackizerTheme.spacing.small),
-        ) {
-            SettingOptionItem(
-                icon = R.drawable.ic_cloud_sync,
-                title = "Cloud Sync",
-                leadingContent = {
-                    TrackizerSwitch(
-                        checked = false,
-                        onCheckedChange = {},
-                    )
-                },
-            )
-        }
+            title = stringResource(R.string.general),
+            options = listOf(
+                SettingOption(
+                    icon = R.drawable.ic_cloud_sync,
+                    title = stringResource(R.string.cloud_sync),
+                    leadingContent = {
+                        TrackizerSwitch(
+                            checked = false,
+                            onCheckedChange = {},
+                        )
+                    },
+                ),
+            ),
+            modifier = Modifier.padding(TrackizerTheme.spacing.small),
+        )
     }
 }
